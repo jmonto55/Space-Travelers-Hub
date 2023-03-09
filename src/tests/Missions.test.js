e50f23a3844e8f2c4ac547d40ae04b1649cb4ac3
anderCM <andercm15@gmail.com>
2023-03-09 17:34:31 -0500

    Eslint errors fixed

--- src/tests/Missions.test.js
+++ src/tests/Missions.test.js
@@ -1,20 +1,19 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import configureMockStore from 'redux-mock-store';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 
 import Missions from '../components/Missions/Missions';
 import store from '../redux/store';
 
 describe('Test suite for Missions Component', () => {
-    it('Render the table header', () => {
-        render(
-            <Provider store={store}>
-                <Missions />
-            </Provider>
-        )
-        expect(screen.getByText(/Mission/i)).toBeInTheDocument();
-        expect(screen.getByText(/Description/i)).toBeInTheDocument();
-        expect(screen.getByText(/Status/i)).toBeInTheDocument();
-    });
-})
+  it('Render the table header', () => {
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+    expect(screen.getByText(/Mission/i)).toBeInTheDocument();
+    expect(screen.getByText(/Description/i)).toBeInTheDocument();
+    expect(screen.getByText(/Status/i)).toBeInTheDocument();
+  });
+});
--- src/tests/MyMissions.test.js
+++ src/tests/MyMissions.test.js
@@ -9,11 +9,9 @@ jest.mock('react-redux');
 
 describe('Test suite for MyMissions component', () => {
   it('should render "No Missions yet" if no missions are joined', () => {
-    useSelector.mockImplementation((selectorFn) =>
-      selectorFn({
-        missions: { missions: [] },
-      })
-    );
+    useSelector.mockImplementation((selectorFn) => selectorFn({
+      missions: { missions: [] },
+    }));
 
     const { getByText } = render(<MyMissions />);
 
@@ -21,17 +19,15 @@ describe('Test suite for MyMissions component', () => {
   });
 
   it('should render the joined missions', () => {
-    useSelector.mockImplementation((selectorFn) =>
-      selectorFn({
-        missions: {
-          missions: [
-            { mission_id: 1, mission_name: 'Mission 1', joined: false },
-            { mission_id: 2, mission_name: 'Mission 2', joined: true },
-            { mission_id: 3, mission_name: 'Mission 3', joined: true },
-          ],
-        },
-      })
-    );
+    useSelector.mockImplementation((selectorFn) => selectorFn({
+      missions: {
+        missions: [
+          { mission_id: 1, mission_name: 'Mission 1', joined: false },
+          { mission_id: 2, mission_name: 'Mission 2', joined: true },
+          { mission_id: 3, mission_name: 'Mission 3', joined: true },
+        ],
+      },
+    }));
 
     const { getByText } = render(<MyMissions />);
 
